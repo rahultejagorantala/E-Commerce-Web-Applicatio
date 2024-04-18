7b11cf3b4f451b702db9b3a8bb8fcadceb07228a
Rahul Teja Gorantala <101026703+rahultejagorantala@users.noreply.github.com>
2024-04-17 19:16:24 -0500

    Delete Backend directory

--- Backend/config/secret.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    secret:'alqpzmxncvbeurpsjfh476878fsk009mfsdo50',
-    admin_email: 'admin@demo.com',
-    admin_password:'password',
-    razorpay_webhook_secret:'MUGOKOLACAFGILSJMLSFHUsbczheUIBB8699'
-    }
-    
